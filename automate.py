afaa2b973591841224550e66d40e82ca1de0b6f0
nkvamsi <36328768+nkvamsi@users.noreply.github.com>
2019-07-02 15:06:32 +0530

    Added Multi Threading

--- automate.py
+++ automate.py
@@ -1,8 +1,9 @@
 from selenium import webdriver
 import time
 import string
+import threading
 
-def search(word):
+def search(browser,word):
     text_area = browser.find_element_by_id('sb_form_q')
     text_area.send_keys(word)
     try:
@@ -11,25 +12,24 @@ def search(word):
         browser.find_element_by_id('sbBtn').click()
     time.sleep(2)
 
-def next():
+def next(browser):
     browser.find_element_by_id('idSIButton9').click()
     time.sleep(2)
 
-def process(email,password):
+def process(browser,email,password):
     browser.find_element_by_id('i0116').send_keys(email)
-    next()
+    next(browser)
 
     browser.find_element_by_id('i0118').send_keys(password)
-    next()
+    next(browser)
 
     for i in string.ascii_lowercase[:26]:
-        search(i)
+        search(browser,i)
     for i in string.ascii_uppercase[:5]:
-        search(i)
+        search(browser,i)
 
 def load(email,password):
     chrome_options = webdriver.ChromeOptions()
-    global browser
     browser = webdriver.Chrome(executable_path = 'chromedriver')
     # browser = webdriver.Safari(executable_path = '/usr/bin/safaridriver')    
     browser.get('https://www.bing.com')
@@ -39,7 +39,7 @@ def load(email,password):
     sign_in.click()
     time.sleep(8)
 
-    process(email,password)
+    process(browser,email,password)
 
     # browser = webdriver.Safari(executable_path = '/usr/bin/safaridriver')
     chrome_options.add_argument('--user-agent="Mozilla/5.0 (iPhone; CPU iPhone OS 12_1_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0 Mobile/15E148 Safari/604.1)"')
@@ -55,10 +55,19 @@ def load(email,password):
     browser.find_element_by_id('hb_s').click()
     time.sleep(4)
 
-    process(email,password)
+    process(browser,email,password)
 
     browser.quit()
 
-load('nkvamsi97@gmail.com','nagarjuna')
-load('kchaitanya863@gmail.com','Kchaitanya123')
-load('nkvamsi97@outlook.com','Krishna!23')
+
+
+user_email = ['nkvamsi97@gmail.com','kchaitanya863@gmail.com','nkvamsi97@outlook.com']
+user_pass = ['nagarjuna','Kchaitanya123','Krishna!23']
+
+t = []
+for i in range(3):
+    t.append(threading.Thread(target=load, args=(user_email[i],user_pass[i],)))
+for i in t:
+    i.start()
+for i in t:
+    i.join()
